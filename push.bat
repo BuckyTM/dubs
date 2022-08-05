6a1cf71a89df6920c71cca610fbe7a342375dc54
Kurumi <ayangkurumi@gmail.com>
2022-08-05 13:08:48 +0700

    Delete push.bat

--- push.bat
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/roshan-iolite/windowd10RDP.git
-git branch -m main
-git status
-git add .
-git status
-git commit -m %1
-git switch main
-git push origin main
