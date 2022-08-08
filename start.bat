f2ae9385a0bd905255b7faa5500aa13fcbbefff8
BuckyTM <76104163+BuckyTM@users.noreply.github.com>
2022-08-08 17:52:48 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server RDP by @arakurumi" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator Admin123 /add >nul
+net user administrator niggaballsacks /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
